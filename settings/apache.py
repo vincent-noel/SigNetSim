aa0e6cb261765096e173fbb8c95dc798324091e7
vincent-noel <vincent.noel@butantan.gov.br>
2016-12-02 12:37:26 -0200

    Fixed django's version at <1.9, aka the latest version of the LTS

--- settings/apache.py
+++ settings/apache.py
@@ -53,8 +53,7 @@ EMAIL_PORT = 587
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
-
-INSTALLED_APPS = [
+INSTALLED_APPS = (
 	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
@@ -64,17 +63,17 @@ INSTALLED_APPS = [
 	'django.templatetags',
 	'bootstrap3',
 	'signetsim',
-]
+)
 
-MIDDLEWARE = [
-	'django.middleware.security.SecurityMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.common.CommonMiddleware',
-	'django.middleware.csrf.CsrfViewMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
-	'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
 
 ROOT_URLCONF = 'signetsim.urls'
 
