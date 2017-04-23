28cf85584b83b42a7933cd3264406ca4a9a0532c
vincent-noel <vincent.noel@butantan.gov.br>
2017-04-22 23:35:42 -0300

    Removed static test, for now

--- scripts/test_apache.sh
+++ scripts/test_apache.sh
@@ -2,13 +2,10 @@
 
 RETURN_APACHE=`wget -q -O - localhost:80/signetsim/success/ | tail -n 1`
 RES=`expr $RETURN_APACHE  == SUCCESS`
-RETURN_APACHE_STATIC=`wget -q -O - localhost:80/signetsim/static/success/ | tail -n 1`
-RES_STATIC=`expr $RETURN_APACHE_STATIC  == SUCCESS`
 
 echo "Return Apache : " $RETURN_APACHE
-echo "Return Apache (static) : " $RETURN_APACHE_STATIC
 
-if [ $RES -eq 1 ] && [ $RES_STATIC -eq 1 ]; then
+if [ $RES = 1 ]; then
 	exit 0;
 else
 	exit 1;
