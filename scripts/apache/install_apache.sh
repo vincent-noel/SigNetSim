ff6cd47828111ae9afe0d7d52b241b16adf69157
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-03 12:34:46 -0300

    Apache install now should be compatible with any versions

--- scripts/apache/install_apache.sh
+++ scripts/apache/install_apache.sh
@@ -4,29 +4,32 @@ T_DIR=`dirname $DIR`
 INSTALL_DIR=`dirname $T_DIR`
 ROOT_DIR=$1
 
-APACHE_ACTIVE_CONF=`ls /etc/apache2/sites-enabled/`
-NB_ACTIVE_CONF=`echo ${APACHE_ACTIVE_CONF} | wc -w`
+APACHE_ACTIVE_CONF_NAME=`ls /etc/apache2/sites-enabled/`
+NB_ACTIVE_CONF=`echo ${APACHE_ACTIVE_CONF_NAME} | wc -w`
+
 
 if [ "${NB_ACTIVE_CONF}" -eq 1 ]
 then
-    cp /etc/apache2/sites-available/${APACHE_ACTIVE_CONF} ${DIR}
-    NB_LINES=`wc -l $DIR/${APACHE_ACTIVE_CONF} | cut -d' ' -f1`
-    POS_ENDVH=`cat $DIR/${APACHE_ACTIVE_CONF} | grep -n /VirtualHost | cut -d: -f1`
+
+    APACHE_ACTIVE_CONF=`realpath /etc/apache2/sites-enabled/$( readlink /etc/apache2/sites-enabled/${APACHE_ACTIVE_CONF_NAME} )`
+    cp ${APACHE_ACTIVE_CONF} ${DIR}
+    NB_LINES=`wc -l $DIR/$( basename ${APACHE_ACTIVE_CONF} ) | cut -d' ' -f1`
+    POS_ENDVH=`cat $DIR/$( basename ${APACHE_ACTIVE_CONF} ) | grep -n /VirtualHost | cut -d: -f1`
     POS_INSERT=`expr $POS_ENDVH - 1`
     LINES_LEFT=`expr $NB_LINES - $POS_INSERT`
 
-    cat $DIR/${APACHE_ACTIVE_CONF} | head -n $POS_INSERT > $DIR/signetsim.conf
+    cat $DIR/$( basename ${APACHE_ACTIVE_CONF} ) | head -n $POS_INSERT > $DIR/signetsim.conf
     cat $DIR/apache_conf >> $DIR/signetsim.conf
-    cat $DIR/${APACHE_ACTIVE_CONF} | tail -n $LINES_LEFT >> $DIR/signetsim.conf
+    cat $DIR/$( basename ${APACHE_ACTIVE_CONF} ) | tail -n $LINES_LEFT >> $DIR/signetsim.conf
 
-    rm $DIR/${APACHE_ACTIVE_CONF} $DIR/apache_conf
+    rm $DIR/$( basename ${APACHE_ACTIVE_CONF} ) $DIR/apache_conf
 
     mv $DIR/signetsim.conf /etc/apache2/sites-available/
     cd /etc/apache2/sites-enabled/
-    a2dissite ${APACHE_ACTIVE_CONF}
+    a2dissite ${APACHE_ACTIVE_CONF_NAME}
     a2enmod wsgi
     a2enmod rewrite
-    a2ensite signetsim
+    a2ensite signetsim.conf
     service apache2 restart
 else
     echo "More than one active configuration in apache."
--- scripts/test_apache.sh
+++ scripts/test_apache.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 RETURN_APACHE=`wget -q -O - localhost:80/signetsim/success | tail -n 1`
-RES=`expr $RETURN_APACHE  == SUCCESS`
+echo "> Apache returned : ${RETURN_APACHE}"
+RES=`expr $RETURN_APACHE == SUCCESS`
 RETURN_APACHE_STATIC=`wget -q -O - localhost:80/signetsim/static/success | tail -n 1`
-RES_STATIC=`expr $RETURN_APACHE_STATIC  == SUCCESS`
+echo "> Apache returned : ${RETURN_APACHE_STATIC}"
+RES_STATIC=`expr $RETURN_APACHE_STATIC == SUCCESS`
 
 echo "Return Apache : " $RETURN_APACHE
 echo "Return Apache static : " $RETURN_APACHE_STATIC
 
-if [ $RES = 1 ] && [ $RES_STATIC = 1 ]; then
+if [ "$RES" = 1 ] && [ "$RES_STATIC" = 1 ]; then
 	exit 0;
 else
 	exit 1;
