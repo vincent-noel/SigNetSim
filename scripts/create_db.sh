eaca922ecc8ec20c226a7231b77cf58dd7682941
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-05 17:19:32 -0300

    Migrations and collect static should be done within the install

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -5,12 +5,12 @@ INSTALL_DIR=`dirname $DIR`
 
 cd ${INSTALL_DIR}
 
-mkdir -p data/db
-mkdir -p data/media
+#mkdir -p data/db
+#mkdir -p data/media
 
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput > /dev/null
+#python manage.py makemigrations --noinput
+#python manage.py migrate --noinput
+#python manage.py collectstatic --noinput > /dev/null
 echo "from signetsim.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
 
 chown -R www-data:www-data data
--- scripts/xenial/install.sh
+++ scripts/xenial/install.sh
@@ -15,14 +15,14 @@ bower --allow-root install
 mkdir -p signetsim/static/mpld3
 cp -r /usr/local/lib/python2.7/dist-packages/mpld3/js/d3.v3.min.js signetsim/static/mpld3/
 cp -r /usr/local/lib/python2.7/dist-packages/mpld3/js/mpld3.v0.3.min.js signetsim/static/mpld3/
-mkdir static
-mkdir data
 
-#mkdir -p data/db
-#mkdir -p data/media
-#python manage.py makemigrations --noinput
-#python manage.py migrate --noinput
-#python manage.py collectstatic --noinput > /dev/null
-#echo "from signetsim.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin')" | python manage.py shell
+
+
+mkdir -p data/db
+mkdir -p data/media
+
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput > /dev/null
 
 cd $EXEC_DIR
