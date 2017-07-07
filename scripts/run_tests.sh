2f44fdb2182e5c9a1cace0666f64b14c17c253da
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-07 16:24:27 -0300

    Fixing run_tests script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,1 +1,18 @@
+CMD=$0
+
+if [ "${CMD:0:1}" == "/" ]
+then
+    # absolute path
+    DIR=`dirname ${CMD}`
+
+else
+    # relative path
+    DIR=`dirname $( realpath $PWD/${CMD} )`
+
+fi
+
+INSTALL_DIR=`dirname $DIR`
+
+cd ${INSTALL_DIR}
+
 coverage run -a manage.py test || exit 1;
