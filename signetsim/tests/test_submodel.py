c30dfc98f4a0316a21d98f9d042460b373d848cf
vincent-noel <vincent.noel@butantan.gov.br>
2017-07-16 23:07:12 -0300

    Added a test for removing submodels and substitutions

---
+++ signetsim/tests/test_submodel.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+""" test_model.py
+
+
+	This file...
+
+
+
+	Copyright (C) 2016 Vincent Noel (vincent.noel@butantan.gov.br)
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as published
+	by the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+from django.test import TestCase, Client, RequestFactory
+from django.contrib.sessions.middleware import SessionMiddleware
+from signetsim.models import User, Project, SbmlModel
+from signetsim.views.ListOfModelsView import ListOfModelsView
+from django.conf import settings
+from os.path import dirname, join
+from shutil import rmtree
+
+class TestSubmodel(TestCase):
+
+	fixtures = ["user_with_project.json"]
+
+	def testCreateModel(self):
+
+		settings.MEDIA_ROOT = "/tmp/"
+
+		user = User.objects.filter(username='test_user')[0]
+		self.assertEqual(len(Project.objects.filter(user=user)), 1)
+		project = Project.objects.filter(user=user)[0]
+
+		self.assertEqual(len(SbmlModel.objects.filter(project=project)), 0)
+
+		c = Client()
+		self.assertTrue(c.login(username='test_user', password='password'))
+
+		response_choose_project = c.get('/project/%s/' % project.folder)
+		self.assertRedirects(response_choose_project, '/models/', status_code=302, target_status_code=200)
+
+		response_choose_project = c.post('/models/', {
+			'action': 'choose_project',
+			'project_id': 0
+		})
+		self.assertEqual(response_choose_project.status_code, 200)
+
+		files_folder = join(dirname(__file__), "files")
+		comp_files_folder = join(files_folder, "comp_model")
+
+		model_filename = join(comp_files_folder, "modelcEvRcX.xml")
+		response_load_submodel_1 = c.post('/models/', {
+			'action': 'load_model',
+			'docfile': open(model_filename, 'r')
+		})
+
+		self.assertEqual(response_load_submodel_1.status_code, 200)
+		self.assertEqual(len(SbmlModel.objects.filter(project=project)), 1)
+
+		model_filename = join(comp_files_folder, "modelEHfev9.xml")
+		response_load_submodel_2 = c.post('/models/', {
+			'action': 'load_model',
+			'docfile': open(model_filename, 'r')
+		})
+
+		self.assertEqual(response_load_submodel_2.status_code, 200)
+		self.assertEqual(len(SbmlModel.objects.filter(project=project)), 2)
+
+
+		model_filename = join(comp_files_folder, "modelI1vrys.xml")
+		response_load_submodel_3 = c.post('/models/', {
+			'action': 'load_model',
+			'docfile': open(model_filename, 'r')
+		})
+
+		self.assertEqual(response_load_submodel_3.status_code, 200)
+		self.assertEqual(len(SbmlModel.objects.filter(project=project)), 3)
+
+		model_filename = join(comp_files_folder, "modelz9xdww.xml")
+
+		response_load_model = c.post('/models/', {
+			'action': 'load_model',
+			'docfile': open(model_filename, 'r')
+		})
+
+		self.assertEqual(response_load_model.status_code, 200)
+		self.assertEqual(len(SbmlModel.objects.filter(project=project)), 4)
+
+		response_choose_model = c.post('/edit/submodels/', {
+			'action': 'choose_model',
+			'model_id': 3
+		})
+		self.assertEqual(response_choose_model.status_code, 200)
+		self.assertEqual(
+			[submodel for submodel in response_choose_model.context['model_submodels']],
+			['Model definition']
+		)
+
+
+		self.assertEqual(
+			[submodel.getNameOrSbmlId() for submodel in response_choose_model.context['list_of_submodels']],
+			['SOS module', 'Ras module', 'MAPK module']
+		)
+
+		self.assertEqual(
+			response_choose_model.context['list_of_submodel_types'],
+			[1,1,1]
+		)
+
+		self.assertEqual(
+			[
+				(sub_type, object_1.getSbmlId(), submodel, object_2.getSbmlId())
+				for sub_type, object_1, submodel, object_2
+				in response_choose_model.context['list_of_substitutions']
+			],
+			[
+				(0, 'compartment_0', ['sos_mod'], 'cell'),
+				(0, 'compartment_0', ['ras_mod'], 'cell'),
+				(0, 'compartment_0', ['mapk_mod'], 'cell'),
+				(0, 'sos', ['sos_mod'], 'sos'),
+				(0, 'sos', ['ras_mod'], 'sos'),
+				(0, 'rasgtp', ['ras_mod'], 'ras_gtp'),
+				(0, 'rasgtp', ['mapk_mod'], 'ras_gtp'),
+				(0, 'erkpp', ['mapk_mod'], 'mapk_pp'),
+				(0, 'erkpp', ['sos_mod'], 'erkpp')
+			]
+		)
+
+		response_delete_submodel = c.post('/edit/submodels/', {
+			'action': 'delete',
+			'submodel_id': 0
+		})
+
+		self.assertEqual(response_delete_submodel.status_code, 200)
+		self.assertEqual(
+			response_delete_submodel.context['getErrors'],
+			['Submodel SOS module is used. Please remove the substitutions first']
+		)
+
+		self.assertEqual(len(response_delete_submodel.context['list_of_submodel_types']),3)
+
+		response_delete_substitution = c.post('/edit/submodels/', {
+			'action': 'delete_substitution',
+			'substitution_id': 8
+		})
+		self.assertEqual(response_delete_substitution.status_code, 200)
+		self.assertEqual(
+			[
+				(sub_type, object_1.getSbmlId(), submodel, object_2.getSbmlId())
+				for sub_type, object_1, submodel, object_2
+				in response_delete_substitution.context['list_of_substitutions']
+			],
+			[
+				(0, 'compartment_0', ['sos_mod'], 'cell'),
+				(0, 'compartment_0', ['ras_mod'], 'cell'),
+				(0, 'compartment_0', ['mapk_mod'], 'cell'),
+				(0, 'sos', ['sos_mod'], 'sos'),
+				(0, 'sos', ['ras_mod'], 'sos'),
+				(0, 'rasgtp', ['ras_mod'], 'ras_gtp'),
+				(0, 'rasgtp', ['mapk_mod'], 'ras_gtp'),
+				(0, 'erkpp', ['mapk_mod'], 'mapk_pp')
+			]
+		)
+
+		response_delete_substitution = c.post('/edit/submodels/', {
+			'action': 'delete_substitution',
+			'substitution_id': 3
+		})
+		self.assertEqual(response_delete_substitution.status_code, 200)
+		self.assertEqual(
+			[
+				(sub_type, object_1.getSbmlId(), submodel, object_2.getSbmlId())
+				for sub_type, object_1, submodel, object_2
+				in response_delete_substitution.context['list_of_substitutions']
+			],
+			[
+				(0, 'compartment_0', ['sos_mod'], 'cell'),
+				(0, 'compartment_0', ['ras_mod'], 'cell'),
+				(0, 'compartment_0', ['mapk_mod'], 'cell'),
+				(0, 'sos', ['ras_mod'], 'sos'),
+				(0, 'rasgtp', ['ras_mod'], 'ras_gtp'),
+				(0, 'rasgtp', ['mapk_mod'], 'ras_gtp'),
+				(0, 'erkpp', ['mapk_mod'], 'mapk_pp'),
+			]
+		)
+		response_delete_substitution = c.post('/edit/submodels/', {
+			'action': 'delete_substitution',
+			'substitution_id': 0
+		})
+		self.assertEqual(response_delete_substitution.status_code, 200)
+
+		self.assertEqual(
+			[
+				(sub_type, object_1.getSbmlId(), submodel, object_2.getSbmlId())
+				for sub_type, object_1, submodel, object_2
+				in response_delete_substitution.context['list_of_substitutions']
+			],
+			[
+				(0, 'compartment_0', ['ras_mod'], 'cell'),
+				(0, 'compartment_0', ['mapk_mod'], 'cell'),
+				(0, 'sos', ['ras_mod'], 'sos'),
+				(0, 'rasgtp', ['ras_mod'], 'ras_gtp'),
+				(0, 'rasgtp', ['mapk_mod'], 'ras_gtp'),
+				(0, 'erkpp', ['mapk_mod'], 'mapk_pp'),
+			]
+		)
+
+		response_delete_submodel = c.post('/edit/submodels/', {
+			'action': 'delete',
+			'submodel_id': 0
+		})
+
+		self.assertEqual(response_delete_submodel.status_code, 200)
+		self.assertEqual(
+			response_delete_submodel.context['getErrors'],
+			[]
+		)
+
+		self.assertEqual(len(response_delete_submodel.context['list_of_submodel_types']), 2)
