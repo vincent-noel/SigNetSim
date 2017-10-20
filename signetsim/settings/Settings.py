bd87d123dc422c7877a5030bb02d9a9fd4a4d702
vincent <vincent.noel@butantan.gov.br>
2017-10-20 21:43:08 -0200

    Switched color scheme to seaborn paster

--- signetsim/settings/Settings.py
+++ signetsim/settings/Settings.py
@@ -23,7 +23,9 @@
 	This file ...
 
 """
-
+import seaborn
 class Settings(object):
-	default_colors = ["#FFB300", "#803E75", "#FF6800", "#A6BDD7", "#C10020", "#CEA262", "#817066", "#007D34", "#F6768E", "#00538A",
-	 "#FF7A5C", "#53377A", "#FF8E00", "#B32851", "#F4C800", "#7F180D", "#93AA00", "#593315", "#F13A13", "#232C16"]
+	default_colors = seaborn.color_palette(palette="pastel").as_hex()
+
+	# default_colors = ["#FFB300", "#803E75", "#FF6800", "#A6BDD7", "#C10020", "#CEA262", "#817066", "#007D34", "#F6768E", "#00538A",
+	#  "#FF7A5C", "#53377A", "#FF8E00", "#B32851", "#F4C800", "#7F180D", "#93AA00", "#593315", "#F13A13", "#232C16"]
