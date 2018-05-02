a48d127d18d6d0a24ea63aa79d4ef243ce2ae3a7
vincent <vincent.noel@butantan.gov.br>
2018-05-02 17:58:55 -0300

    Adding python3 tests for xenial and stretch

--- travis_run.sh
+++ travis_run.sh
@@ -3,61 +3,65 @@
 if [ $1 = "docker" ]; then
     if [ $2 = "before_install" ]; then
         pip install -U docker-compose || exit 1;
-        docker login -u signetsim -p $3
+        docker login -u signetsim -p $4
 
     elif [ $2 = "install" ]; then
-        docker-compose build signetsim || exit 1;
+        if [ $3 = "2" ]; then
+            docker-compose build signetsim || exit 1;
+        else
+            docker-compose build signetsim-python3 || exit 1;
+        fi
 
     elif [ $2 = "script" ]; then
-        docker run --name signetsim -p 80:80 -d signetsim/signetsim:develop || exit 1;
+        if [ $3 = "2" ]; then
+            docker run --name signetsim -p 80:80 -d signetsim/signetsim:develop || exit 1;
+        else
+            docker run --name signetsim -p 80:80 -d signetsim/signetsim:develop-python3 || exit 1;
+        fi
+
         APACHE_RETURN=`wget -q -O - localhost:80 | grep \<title\> | cut -d">" -f2 | cut -d" " -f1`
         exit `expr ${APACHE_RETURN} != Install`
 
     elif [ $2 = "after_script" ]; then
-        docker push signetsim/signetsim:develop || exit 1;
+        if [ $3 = "2" ]; then
+            docker push signetsim/signetsim:develop || exit 1;
+        else
+            docker push signetsim/signetsim:develop-python3 || exit 1;
+        fi
 
     fi
 
 
 else
-    if [ $1 = "docker-python3" ]; then
-        if [ $2 = "before_install" ]; then
-            pip install -U docker-compose || exit 1;
-            docker login -u signetsim -p $3
 
-        elif [ $2 = "install" ]; then
-            docker-compose build signetsim-python3 || exit 1;
-
-        elif [ $2 = "script" ]; then
-            docker run --name signetsim -p 80:80 -d signetsim/signetsim:develop-python3 || exit 1;
-            APACHE_RETURN=`wget -q -O - localhost:80 | grep \<title\> | cut -d">" -f2 | cut -d" " -f1`
-            exit `expr ${APACHE_RETURN} != Install`
-
-        elif [ $2 = "after_script" ]; then
-            docker push signetsim/signetsim:develop-python3 || exit 1;
-
+    if [ $2 = "before_install" ]; then
+        if [ $3 = "2" ]; then
+            docker pull signetsim/travis_testenv:$1 || exit 1;
+        else
+            docker pull signetsim/travis_testenv:$1-python3 || exit 1;
         fi
 
-    else
-        if [ $2 = "before_install" ]; then
-            docker pull signetsim/travis_testenv:$1 || exit 1;
-
-        elif [ $2 = "install" ]; then
+    elif [ $2 = "install" ]; then
+        if [ $3 = "2" ]; then
             docker run -di --name test_env -v $(pwd):/home/travis/build/vincent-noel/SigNetSim signetsim/travis_testenv:$1 bash
             docker exec test_env chown -R www-data:www-data /home/travis/build/vincent-noel/SigNetSim
             docker exec test_env /bin/bash /home/travis/build/vincent-noel/SigNetSim/scripts/install.sh
-
-        elif [ $2 = "script" ]; then
-            docker exec -u www-data test_env /bin/bash /home/travis/build/vincent-noel/SigNetSim/scripts/test_apache.sh
-            docker exec -u www-data test_env /bin/bash /home/travis/build/vincent-noel/SigNetSim/scripts/run_tests.sh
-
-        elif [ $2 = "after_script" ]; then
-            coveralls
-
         else
-            exit 0;
-
+            docker run -di --name test_env -v $(pwd):/home/travis/build/vincent-noel/SigNetSim signetsim/travis_testenv:$1-python3 bash
+            docker exec test_env chown -R www-data:www-data /home/travis/build/vincent-noel/SigNetSim
+            docker exec test_env /bin/bash /home/travis/build/vincent-noel/SigNetSim/scripts/install-python3.sh
         fi
 
+    elif [ $2 = "script" ]; then
+        docker exec -u www-data test_env /bin/bash /home/travis/build/vincent-noel/SigNetSim/scripts/test_apache.sh
+        docker exec -u www-data test_env /bin/bash /home/travis/build/vincent-noel/SigNetSim/scripts/run_tests.sh
+
+    elif [ $2 = "after_script" ]; then
+        coveralls
+
+    else
+        exit 0;
+
     fi
+
 fi
